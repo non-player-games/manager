3d9e080b53ff79b73d036556106e67540fa1cb38
Eric Liao <rcliao01@gmail.com>
2016-07-06 23:45:51 -0700

    update manager testing json

--- manager.go
+++ manager.go
@@ -1,7 +1,37 @@
 package manager
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
+// A simple hello function to test working
 func Hello() {
 	fmt.Println("Hello manager")
+}
+
+// Action is DTO (data transfer object) between game engine and manager
+type Action struct {
+	Type    string `json:"type"`
+	Payload string `json:"payload"`
+}
+
+// Player struct to indicate each player differently
+type Player struct {
+	PID  string `json:"pid"`
+	Name string `json:"name"`
+}
+
+// RunGame is the main method to manage the game life cycle between bots and game engine
+func RunGame(engineURL string) {
+	// gameEngine := make(chan string)
+	// until game ends
+	for {
+		onCreate := &Action{
+			Type:    "onCreate",
+			Payload: "",
+		}
+		onCreateJSON, _ := json.Marshal(onCreate)
+		fmt.Println(string(onCreateJSON))
+	}
 }
